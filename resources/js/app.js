485822a0a39d62e3333ab9bdb285568bce33e12f
Aleksandar Andric <aleksandar.andric@sportion.net>
2021-03-27 16:24:16 +0100

    initial commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,6 @@
-require('./bootstrap');
-
-require('alpinejs');
+require("./bootstrap");
+require("alpinejs");
+import * as $ from "jquery";
+$(document).ready(function () {
+    console.log("Document is ready!");
+});
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\CandidateController;
+use App\Http\Controllers\RecruitmentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +25,11 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::resource('company', CompanyController::class);
+    Route::resource('candidate', CandidateController::class);
+    Route::resource('job', JobController::class);
+    Route::resource('note', NoteController::class);
+    Route::resource('recruitment', RecruitmentController::class);
+});
